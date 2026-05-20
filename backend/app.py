58e9f2ff853d159b47c9a21cbbd2c13190dc24f1
Tanveer Abbas <tanveerkhoja786@gmil.com>
2026-05-20 12:37:42 +0500

    Fix backend import for Vercel

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,8 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(__file__))
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
